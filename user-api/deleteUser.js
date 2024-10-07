321d12e14e23847aba6acb6c18da00ffa6156b0e
sibbe23 <sibbe23@gmail.com>
2024-10-07 09:43:25 +0530

    debug done

--- user-api/deleteUser.js
+++ user-api/deleteUser.js
@@ -3,30 +3,26 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = async (event) => {
   const requestBody = JSON.parse(event.body);
-  const {UserId, UserName,UserEmail} = requestBody;
+  const { UserId, UserName } = requestBody;
 
-  const params ={
-    TableName : process.env.USER_TABLE,
-    Key :{
-        UserId : UserId,
-        UserName:UserName,
-        UserEmail: UserEmail
+  const params = {
+    TableName: process.env.USER_TABLE,
+    Key: {
+      UserId: UserId,
+      UserName: UserName
     }
-  }
+  };
 
-  try{
+  try {
     await dynamoDb.delete(params).promise();
     return {
-        statusCode : 200,
-        body : JSON.stringify({message: 'User data deleted successfully!'})
-    }
+      statusCode: 200,
+      body: JSON.stringify({ message: 'User data deleted successfully!' })
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ err: `Could not delete user: ${err.message}` })
+    };
   }
-  catch(err)
-  {
-    return{
-        statusCode: 500,
-        body :JSON.stringify({err:`Could not delete user : ${err.message}`})
-    }
-  }
-
-}
+};
