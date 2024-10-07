74a2682ea7eeabc3fdc972629a0aebba1ac8f236
sibbe23 <sibbe23@gmail.com>
2024-10-07 09:42:54 +0530

    debug done

--- user-api/updateUser.js
+++ user-api/updateUser.js
@@ -3,34 +3,31 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = async (event) => {
   const requestBody = JSON.parse(event.body);
-  const {UserId, UserStatus, UserEmail, UserName} = requestBody;
+  const { UserId, UserStatus, UserName } = requestBody;
 
   const params = {
-    TableName : process.env.USER_TABLE,
-    Key :{
-        UserId : UserId,
-        UserName : UserName,
-        UserEmail : UserEmail,
+    TableName: process.env.USER_TABLE,
+    Key: {
+      UserId: UserId,
+      UserName: UserName
     },
-    UpdateExpression : 'SET UserStatus = :status',
-    ExpressionAttributeValues : {
-        ':status' : UserStatus
-    }
-  }
+    UpdateExpression: 'SET UserStatus = :status',
+    ExpressionAttributeValues: {
+      ':status': UserStatus
+    },
+    ReturnValues: 'ALL_NEW' // To return the updated attributes
+  };
 
   try {
-    await dynamoDb.update(params).promise();
-    return{
-        statusCode : 200,
-        body : JSON.stringify({message: 'User updated successfully!'})
-
-    }
+    const result = await dynamoDb.update(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'User updated successfully!', updatedAttributes: result.Attributes })
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ err: `Could not complete request: ${err.message}` })
+    };
   }
-  catch(err)
-  {
-    return{
-        statusCode : 500,
-        body : JSON.stringify({err:`Could not complete request : ${err.message}`})
-    }
-  }
-}
+};
